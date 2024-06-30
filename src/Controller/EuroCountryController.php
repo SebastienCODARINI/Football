c64c325eee24dab109bf25e1e078d2f408015f61
Sebastien Codarini <sebastien.codarini@oclock.school>
2024-06-30 17:32:23 +0200

    pages dynamic ok

--- src/Controller/EuroCountryController.php
+++ src/Controller/EuroCountryController.php
@@ -2,23 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\CountryRepository;
 use App\Repository\EuroCountryRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EuroCountryController extends AbstractController
 {
     /**
      * @Route("/euro/countries", name="app_euro_country")
      */
-    public function list(EuroCountryRepository $EuroCountryRepository): Response
+    public function list(EuroCountryRepository $EuroCountryRepository, CountryRepository $CountryRepository): Response
     {
         $eurocountries = $EuroCountryRepository->findAll();
+        $countries = $CountryRepository->findAll();
         
         return $this->render('euro_country/list.html.twig', [
             'controller_name' => 'EuroCountryController',
-            'eurocountries' => $eurocountries
+            'eurocountries' => $eurocountries,
+            'countries' => $countries
         ]);
     }
 }
